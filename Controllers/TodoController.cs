d8926d6f69046016f196cf0fa8d4710429775e04
Rao Taha <raotaha34@gmail.com>
2025-12-17 00:21:00 +0500

    Add files via upload

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Data;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Todo
+        public async Task<IActionResult> Index()
+        {
+            var todos = await _context.Todos.ToListAsync();
+            return View(todos);
+        }
+
+        // GET: Todo/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Todo/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Task,IsCompleted,CreatedAt")] Todo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                todo.CreatedAt = DateTime.Now;
+                _context.Add(todo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(todo);
+        }
+
+        // GET: Todo/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var todo = await _context.Todos.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            return View(todo);
+        }
+
+        // POST: Todo/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Task,IsCompleted,CreatedAt")] Todo todo)
+        {
+            if (id != todo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(todo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Todos.Any(e => e.Id == todo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(todo);
+        }
+
+        // GET: Todo/Delete/5
+        // GET: Todo/Delete/5
+public async Task<IActionResult> Delete(int? id)
+{
+    if (id == null) return NotFound();
+
+    var todo = await _context.Todos.FirstOrDefaultAsync(m => m.Id == id);
+    if (todo == null) return NotFound();
+
+    return View(todo); // This must match the Delete.cshtml view
+}
+
+// POST: Todo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+            if (todo == null) return NotFound();
+
+            _context.Todos.Remove(todo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
+
