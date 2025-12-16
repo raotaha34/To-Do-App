3ac32c9f830ea1c7d6e458a5c401cdb0295a8b58
Rao Taha <raotaha34@gmail.com>
2025-12-17 00:21:34 +0500

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;  // Make sure Todo.cs exists in this namespace
+
+namespace TodoApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Constructor that passes options to the base DbContext class
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet represents a collection of Todo objects (mapped to the Todos table in the database)
+        public DbSet<Todo> Todos { get; set; } = null!;
+        // The = null!; tells C# the property will be initialized by EF Core, avoiding nullable warnings
+    }
+}
