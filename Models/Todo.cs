9003fab64f2a2b2b4c9779eb1ca34e044d51b979
Rao Taha <raotaha34@gmail.com>
2025-12-17 00:21:50 +0500

    Add files via upload

---
+++ Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public required string Task { get; set; } // or string? Task if nullable
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
